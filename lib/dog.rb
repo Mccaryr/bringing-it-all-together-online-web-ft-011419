0938eb6a7e04b37fb1cf605da47aec1b2b78ad8a
Robert McCary <robert.mccary@yahoo.com>
2019-02-16 19:08:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -22,9 +22,9 @@ class Dog
   end
 
   def save
-    if self.id
-      self.update
-    else
+    # if self.id
+    #   self.update
+    # else
       sql = "INSERT INTO dogs (name, breed)
       VALUES (?,?);"
 
