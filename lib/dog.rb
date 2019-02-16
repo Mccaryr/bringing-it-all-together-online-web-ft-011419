ee260022f0ec68e7dcc5db56e01c36cbeeb555dc
Robert McCary <robert.mccary@yahoo.com>
2019-02-16 23:30:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,7 +42,8 @@ class Dog
 
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs
-    WHERE id = ?"
+    WHERE id = ?
+    LIMIT 1"
     result = DB[:conn].execute(sql,id)[0]
     Dog.new(result[0], result [1], result[2])
   end
