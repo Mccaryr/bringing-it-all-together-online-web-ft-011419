c292fdd668eb75d402ce49604e34807a0a487230
Robert McCary <robert.mccary@yahoo.com>
2019-02-16 19:03:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,4 +7,13 @@ class Dog
     @breed=breed
     @id=id
   end
+
+  def self.create_table
+    sql = "CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+    );"
+    DB[:conn].execute(sql)
+  end
 end
